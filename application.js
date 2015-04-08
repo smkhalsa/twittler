944818e6d520df94893e2f6eb217fab90b2d2e1a
Sat Mandir Khalsa <smkhalsa@gmail.com>
2015-04-08 15:59:05 -0700

    Link added to load new tweets

--- application.js
+++ application.js
@@ -1,15 +1,34 @@
-/*$(document).ready(function(){
-  var $body = $('body');
-  $body.html('');
+$(document).ready(function(){
 
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    var $tweet = $('<div></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($body);
-    index -= 1;
-  }
+  var index = 0;
+
+  var loadTweets = function() {
+    while(streams.home.length > index){
+      var tweet = streams.home[index];
+      var $tweet = $('<pre></pre>');
+      $tweet.html('@' + tweet.user + ': ' + tweet.message + tweet.created_at);
+      $tweet.prependTo(".tweets");
+      index += 1;
+    }
+  };
+
+  loadTweets();
+
+  /*
+  $("#tweet-input").on("click", "button", function(){
+    var thisTweet = $(this).closest("#tweet-input").find("input");
+    console.log(thisTweet.val());
+    writeTweet(thisTweet.val());
+    thisTweet.reset();
+  });
+  */
+
+  window.setInterval(function() {
+    $("#numNew").text(streams.home.length-index)
+  }, 100);
+
+  $(".alert-link").on("click", function(){
+    loadTweets();
+  });
 
 });
-*/
